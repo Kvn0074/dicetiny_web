f11cab1d92648ff9cde8d83dd809936e30a36c50
Kevin <kevindelaunay74@gmail.com>
2022-01-27 15:14:06 +0100

    complete secudary function and add DOM events
    creations of the index of script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+/*==============================================================
+=========  INDEX  ==============================================
+================================================================
+
+    1 - Class initialization
+    2 - Variables initialization
+    3 - DOM elements recuperations
+    4 - Secundary functions
+    5 - Prymary function
+    6 - DOM events
+    
+================================================================
+================================================================
+==============================================================*/
+
+
 /*= 1 ==========================================================
 =========  CLASS INIT  =========================================
 ==============================================================*/
@@ -44,6 +60,15 @@ let current = document.getElementById('current');
 let global_one = document.getElementById('global_one');
 let global_two = document.getElementById('global_two');
 let cube = document.querySelector('.cube');
+let fly_spec = document.getElementById('flying_spec');
+let fly_victory = document.getElementById('flying_victory');
+let winner_is = document.getElementById('the_winner_is');
+let victory_sound = document.getElementById('victory_sound');
+let music = document.getElementById('music');
+let speak_img = document.getElementById('speaker');
+let playerOne = document.getElementById('player_one_name');
+let playerTwo = document.getElementById('player_two_name');
+let opsy = document.getElementById('opsy_daisy');
 
 /*= 4 ==========================================================
 =========  SECONDARY FUNCTIONS  ================================
@@ -52,6 +77,61 @@ let cube = document.querySelector('.cube');
 function getRandomInt() {
     return Math.floor(Math.random() * 6) + 1;
   }
+
+function newgame(){
+    joueur1.round  = 0;
+    joueur1.global = 0;
+    joueur1.currentPlayer = true;
+    joueur2.round  = 0;
+    joueur2.global = 0;
+    joueur2.currentPlayer = false;
+    current.textContent = " ";
+    global_one.textContent  = " ";
+    global_two.textContent  = " ";
+
+    if (currentClass) {
+        cube.classList.remove( currentClass );
+    }
+
+    cube.classList.add('show-1');
+    fly_victory.className = 'disappear';
+    lightName();
+}
+
+function add_spec(){
+    fly_spec.className = 'appear';
+}
+
+function none_spec(){
+    fly_spec.className = 'disappear';
+}
+
+function add_victory(joueur){
+    fly_victory.className = 'appear';
+    winner_is.textContent = joueur.name;
+    victory_sound.play();
+}
+
+function add_music(){
+    if(music.paused){
+        speak_img.src = "assets/pics/Speaker_ok.png";
+        music.play();
+    } else {
+        speak_img.src = "assets/pics/speaker_none.png";
+        music.pause();
+    }
+}
+
+function lightName(){
+    if(joueur1.currentPlayer){
+        playerOne.className = "neon";
+        playerTwo.className  = "";
+    }
+    else{
+        playerTwo.className  = "neon";
+        playerOne.className = "";
+    };
+};
 
 /*= 5 ==========================================================
 =========  PRIMARY FUNCTIONS  ==================================
@@ -98,3 +178,26 @@ function rolldice(joueurA, joueurB){
                   }
           }
   }
+
+/*= 6 ==========================================================
+=========  DOM EVENTS  =========================================
+==============================================================*/
+    
+hold_btn.addEventListener('click', function(){
+    if(joueur1.currentPlayer){
+      joueur1.hold(joueur2);
+      current.textContent = " ";
+      global_one.textContent = joueur1.global;
+    } 
+    else {
+      joueur2.hold(joueur1);
+      current.textContent = " ";
+      global_two.textContent = joueur2.global;
+    }
+  });
+
+  restart_btn.addEventListener('click', newgame);
+
+/*==============================================================
+================================================================
+==============================================================*/
